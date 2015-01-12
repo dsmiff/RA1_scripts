baf390c89903b288f8e6ec7e77b6a1788de9fbf5
Robin Aggleton <robinaggleton@gmail.com>
2015-01-12 19:00:55 +0100

    Pu tinto one script now, should work

--- shape_plots.py
+++ shape_plots.py
@@ -1,10 +1,9 @@
 """
-
 This script makes data VS background plots, but gets the backgrounds from
 DATA control region shape, not MC. To do this, we define several control regions
 (for each BG source), use MC to calculate transfer factors, then scale the
 data control region plot by that factor. Oh yeah and whack in stat + syst
-uncertainties, latter from closure tests.
+uncertainties, latter from closure tests. (NB syst invalid for fine jet multiplicity)
 
 We do this for bins of Njets, Nbtag, HT. And we look at lots of variables.
 
@@ -22,6 +21,9 @@ import numpy as np
 import os
 import array
 from Ratio_Plot import Ratio_Plot
+import sys
+import make_component_pres as pres
+
 
 r.TH1.SetDefaultSumw2(r.kFALSE)
 r.gStyle.SetOptStat(0)
@@ -29,12 +31,24 @@ r.gROOT.SetBatch(1)
 r.gStyle.SetOptFit(1111)
 # r.TH1.AddDirectory(r.kFALSE)
 
-# input files & output directories
-ROOTdir, out_dir = [
-    ["/Users/robina/Dropbox/AlphaT/Root_Files_11Dec_aT_0p53_forRobin_v0/", "11Dec_aT_0p53_forRobin_v0"],  #re-run
-    ["/Users/robina/Dropbox/AlphaT/Root_Files_01Dec_aT_0p53_globalAlphaT_v1", "./01Dec_aT_0p53_globalAlphaT_v1/"],  # alphaT in control regions as well
-    ["/Users/robina/Dropbox/AlphaT/Root_Files_04Dec_aT_0p53_fullHT_dPhi_lt0p3_v0", "./04Dec_aT_0p53_fullHT_dPhi_lt0p3_v0/"]  # dPhi* <0.3 in SR
-][0]
+# Define region bins
+allHTbins = ["200_275", "275_325", "325_375", "375_475", "475_575",
+          "575_675", "675_775", "775_875", "875_975", "975_1075", "1075"][:]
+# n_j = ["le3j", "ge4j", "ge2j"][:2]
+n_j = ["eq2j", "eq3j", "eq4j", "ge5j"] # fine jet binning
+n_b = ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b", "ge1b"][:2]
+
+
+# input files, output directories, which HTbins to run over
+# if fineJetMulti need to use fine_jet branch, else master branch. Need better way.
+ROOTdir, out_dir, HTbins = [
+    ["/Users/robina/Dropbox/AlphaT/Root_Files_11Dec_aT_0p53_forRobin_v0/", "11Dec_aT_0p53_forRobin_v0_new", allHTbins[:]],  #re-run
+    ["/Users/robina/Dropbox/AlphaT/Root_Files_11Dec_aT_0p53_forRobin_v0_MuonInJet/", "11Dec_aT_0p53_forRobin_v0_MuonInJet", allHTbins[3:]],  # muon in jet
+    ["/Users/robina/Dropbox/AlphaT/Root_Files_01Dec_aT_0p53_globalAlphaT_v1", "./01Dec_aT_0p53_globalAlphaT_v1/", allHTbins[3:]],  # alphaT in control regions as well
+    ["/Users/robina/Dropbox/AlphaT/Root_Files_04Dec_aT_0p53_fullHT_dPhi_lt0p3_v0", "./04Dec_aT_0p53_fullHT_dPhi_lt0p3_v0/", allHTbins[:]],  # dPhi* <0.3 in SR - old
+    ["/Users/robina/Dropbox/AlphaT/Root_Files_21Dec_alphaT_0p53_fullHT_fixedCC_fineJetMulti_dPhi_gt0p3_v0", "./21Dec_alphaT_0p53_fullHT_fixedCC_fineJetMulti_dPhi_gt0p3_v0_new/", allHTbins[:]],  # dPhi* >0.3 in SR, fine jet multiplicity, fixed cross-cleaner
+    ["/Users/robina/Dropbox/AlphaT/Root_Files_21Dec_alphaT_0p53_fullHT_fixedCC_fineJetMulti_dPhi_lt0p3_v0", "./21Dec_alphaT_0p53_fullHT_fixedCC_fineJetMulti_dPhi_lt0p3_v0_new/", allHTbins[:]]  # dPhi* <0.3 in SR, fine jet multiplicity, fixed cross-cleaner
+][-1]
 
 # Variable(s) you want to plot
 plot_vars = ["Number_Btags", "AlphaT", "LeadJetPt", "LeadJetEta",
@@ -42,265 +56,65 @@ plot_vars = ["Number_Btags", "AlphaT", "LeadJetPt", "LeadJetEta",
              "MHTovMET", "ComMinBiasDPhi_acceptedJets", "EffectiveMass",
              "Number_Good_verticies", "JetMultiplicity"]
 
-out_stem = "plot"
+# Custom bins for AlphaT per Rob's suggestion
+alphaT_bins = np.concatenate((np.arange(0.5, 1.0, 0.05), np.arange(1.0, 5.5, 0.5)))
 
-# Define region bins
-HTbins = ["200_275", "275_325", "325_375", "375_475", "475_575",
-          "575_675", "675_775", "775_875", "875_975", "975_1075", "1075"]
-ht_scheme = ["incl", "excl"][0]  # option for individual bin, or inclusive - not used ATM
-n_j = ["le3j", "ge4j", "ge2j"][:2]
-n_b = ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b", "ge1b"][:2]
+dphi_bins = np.arange(0.0, 4.1, 0.1)
 
-sel=["OneMuon", "DiMuon", "Had"]
-# # MC processes that go into transfer factors
-# processes_mc_ctrl = ['DY', 'DiBoson', 'TTbar', 'WJets', 'Zinv', 'SingleTop']
+rebin_d = {"Number_Btags": 1, "JetMultiplicity": 1, "MHTovMET": 1,
+            "ComMinBiasDPhi_acceptedJets": dphi_bins, "AlphaT": alphaT_bins,
+            "MET_Corrected": 8, "HT": 10, "LeadJetPt": 5, "SecondJetPt": 5,
+            "EffectiveMass": 10, "MHT": 4}
 
-# processes_mc_signal_le1b = {"OneMuon": ['DY', 'DiBoson', 'TTbar', 'WJets', 'SingleTop'],
-#                             "DiMuon": ['Zinv']}
-
-# processes_mc_signal_ge2b = {"OneMuon": ['DY', 'DiBoson', 'TTbar', 'WJets', 'SingleTop', 'Zinv'],
-#                             "DiMuon": []}  # for >= 2btags dimu region not used
-
-# # Control regions to get data shapes (+ proper titles for legend etc)
-# ctrl_regions = {"OneMuon": "Single #mu BG", "DiMuon": "#mu#mu BG"}
-
-# # Sytematics on TF (as a %). Fn on njets & HT
-# tf_systs = {
-#     "le3j": {"200_275": 4, "275_325": 6, "325_375": 6, "375_475": 8, "475_575": 8, "575_675": 12, "675_775": 12, "775_875": 17, "875_975": 17, "975_1075": 19, "1075": 19},
-#     "ge4j": {"200_275": 6, "275_325": 6, "325_375": 11, "375_475": 11, "475_575": 11, "575_675": 18, "675_775": 18, "775_875": 20, "875_975": 20, "975_1075": 26, "1075": 26}
-# }
+log_these = ["AlphaT", "ComMinBiasDPhi_acceptedJets", "HT", "LeadJetPt", "SecondJetPt", "EffectiveMass"]
 
 
-def make_plot_bins(var):#, njet, nbtag, ht):
-    """
-    For a given variable, makes data VS background plots for all the
-    relevant HT, Njets, Nbtag bins
-    """
-    # Custom bins for AlphaT per Rob's suggestion
-    b1 = np.arange(0.5, 1.0, 0.05)
-    b2 = np.arange(1.0, 4.5, 0.5)
-    alphaT_bins = np.concatenate((b1, b2))
-    # alphaT_bins = 20 # an alternate sensible equal bin value
+def do_a_plot_HT_incl(var="ComMinBiasDPhi_acceptedJets", njet="eq3j", btag="eq0b"):
+    # Inclusive HT
+    rebin = rebin_d[var] if var in rebin_d else 2
+    log = True if var in log_these else False
+    plot = Ratio_Plot(ROOTdir, out_dir, var, njet, btag, HTbins, rebin, log)
+    plot.make_plots()
+    plot.save()
 
 
-    # ONLY DO ONE OF THE FOLLOWING - IF YOU TRY AND DO ALL IT'LL SEGFAULT
-    # for v, njet, btag, ht in product(var, n_j, n_b, HTbins):
-    for v in var:
-        print "Doing plots for", v
-        rebin = 2
-        rebin_d = {"Number_Btags": 1, "JetMultiplicity": 1, "MHTovMET": 1,
-                    "ComMinBiasDPhi_acceptedJets": 10, "AlphaT": alphaT_bins,
-                    "MET_Corrected": 8, "HT": 1, "SecondJetPt": 1, "EffectiveMass": 5,
-                    "MHT": 4}
-        if v in rebin_d:
-            rebin = rebin_d[v]
-        log = False
-        if v in ["AlphaT", "ComMinBiasDPhi_acceptedJets"]: #, "HT"]:
-            log = True
-        plot = Ratio_Plot(ROOTdir, "plot", v, "le3j", "eq0b", ["325_375"], rebin, log)
-        # print plot.hist_data_signal
-        print "YO"
-        plot.make_plots()
-        outd = "%s/%s_%s_%s" %(out_dir, "le3j", "eq0b", "325_375")
-        plot.save(odir=outd)
-        # dave = grabr.grab_plots("/Users/robina/Dropbox/AlphaT/Root_Files_11Dec_aT_0p53_forRobin_v0/Muon_Data.root",sele="OneMuon", h_title=v, njet=njet, btag=btag, ht_bins=ht)
-        # print dave.GetName()
-        # plot = Ratio_Plot(ROOTdir, "plot", v, "le3j", "eq1b", ["675_775"], rebin, log)
-        # plot.make_plots()
-        # outd = "%s/%s_%s_%s" %(out_dir, "le3j", "eq1b", "675_775")
-        # plot.save(odir=outd)
-
-
-    # For ge4j cos the binning is diff in cases...
-    for v in var:
-        print "Doing plots for", v
-        rebin = 2
-        rebin_d = {"Number_Btags": 1, "JetMultiplicity": 1, "MHTovMET": 1,
-                    "ComMinBiasDPhi_acceptedJets": 10, "AlphaT": alphaT_bins,
-                    "MET_Corrected": 8, "HT": 1, "SecondJetPt": 1, "EffectiveMass": 5,
-                    "MHT": 8}
-        if v in rebin_d:
-            rebin = rebin_d[v]
-        log = False
-        if v in ["AlphaT", "ComMinBiasDPhi_acceptedJets"]: #, "HT"]:
-            log = True
-        # plot = Ratio_Plot(v, "ge4j", "eq0b", ["675_775"], rebin, log)
-        # outd = "%s/%s_%s_%s" %(out_dir, "ge4j", "eq0b", "675_775")
-        # plot.save(odir=outd)
-
-        # plot = Ratio_Plot(v, "ge4j", "eq1b", ["675_775"], rebin, log)
-        # outd = "%s/%s_%s_%s" %(out_dir, "ge4j", "eq1b", "675_775")
-        # plot.save(odir=outd)
-
-
-    # For inclusive HT
-    counter = 0
-    for v in var[:]:
-        print "Doing plots for", v
-        rebin = 2
-        rebin_d = {"Number_Btags": 1, "JetMultiplicity": 1, "MHTovMET": 1,
-                    "ComMinBiasDPhi_acceptedJets": 10, "AlphaT": alphaT_bins,
-                    "MET_Corrected": 8, "HT": 5, "SecondJetPt": 4, "EffectiveMass": 10,
-                    "MHT": 8, "LeadJetPt": 4}
-        if v in rebin_d:
-            rebin = rebin_d[v]
-
-        log = False
-        if v in ["AlphaT", "ComMinBiasDPhi_acceptedJets", "HT", "LeadJetPt", "SecondJetPt", "EffectiveMass"]:
-            log = True
-
-        njet = "ge4j"
-        btag = "eq0b"
-        counter += 1
-        # plot = Ratio_Plot(ROOTdir, "plot", v, njet, btag, HTbins, rebin, log)
-        # plot.make_plots()
-        # outd = "%s/%s_%s_%s" %(out_dir, njet, btag, "200_Inf")
-        # plot.save(odir=outd)
-        # del plot
-        # print counter
-
-        # njet = "ge4j"
-        # btag = "eq1b"
-        # counter += 1
-        # plot = Ratio_Plot(ROOTdir, "plot", v, njet, btag, HTbins, rebin, log)
-        # plot.make_plots()
-        # outd = "%s/%s_%s_%s" %(out_dir, njet, btag, "200_Inf")
-        # plot.save(odir=outd)
-        # del plot
-        # print counter
-    # Testing
-    # plot = Ratio_Plot("AlphaT", "le3j", "eq0b", "375_475", 20, True)
-    # plot.save()
-    # plot = Ratio_Plot("JetMultiplicity", "le3j", "eq0b", "375_475", 1, False)
-    # plot.save()
-    # plot = Ratio_Plot("LeadJetPt", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("SecondJetPt", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("HT", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("SecondJetEta", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("LeadJetEta", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("MHT", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("MET_Corrected", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("MHTovMET", "ge4j", "eq0b", "375_475", 2, True)
-    # plot.save()
-    # plot = Ratio_Plot("ComMinBiasDPhi_acceptedJets", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("EffectiveMass", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("Number_Btags", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-    # plot = Ratio_Plot("Number_Good_verticies", "le3j", "eq0b", "375_475", 2, False)
-    # plot.save()
-
-
-def do_all_plots_HT_excl():
-    """
-    The big enchilada
-    """
-    # Custom bins for AlphaT per Rob's suggestion
-    b1 = np.arange(0.5, 1.0, 0.05)
-    b2 = np.arange(1.0, 4.5, 0.5)
-    alphaT_bins = np.concatenate((b1, b2))
-
-    # exclusive HT bins
-    rebin_d = {"Number_Btags": 1, "JetMultiplicity": 1, "MHTovMET": 1,
-                "ComMinBiasDPhi_acceptedJets": 10, "AlphaT": alphaT_bins,
-                "MET_Corrected": 8, "HT": 1, "SecondJetPt": 1, "EffectiveMass": 5,
-                "MHT": 4}
-    log_these = ["AlphaT", "ComMinBiasDPhi_acceptedJets"] #, "HT"]:
-
+def do_a_plot_HT_excl(var="AlphaT", njet="le3j", btag="eq0b"):
+    # exclusive HT bins - do one by one
     for ht in HTbins:
-        v = plot_vars[6]
-    # for njet, btag, ht in product(n_j, n_b, HTbins):
-        if v in rebin_d:
-            rebin = rebin_d[v]
-        else:
-            rebin = 2
-        if v in log_these:
-            log = True
-        else:
-            log = False
-
-        # print  v, njet, btag, [ht]
-        njet = "le3j"
-        btag = "eq0b"
-        plot = Ratio_Plot(ROOTdir, out_stem, v, njet, btag, [ht], rebin, log)
+        rebin = rebin_d[var] if var in rebin_d else 2
+        log = True if var in log_these else False
+        plot = Ratio_Plot(ROOTdir, out_dir, var, njet, btag, [ht], rebin, log)
+        plot.plot_components = True
         plot.make_plots()
-        outd = "%s/%s_%s_%s" %(out_dir, njet, btag, ht)
-        plot.save(odir=outd)
-
-        # plot = Ratio_Plot("Number_Btags", njet, btag, [ht], 1, False)
-        # plot.save(odir=outd)
-        # plot = Ratio_Plot("AlphaT", njet, btag, [ht], alphaT_bins, True)
-        # plot.save(odir=outd)
-        # # plot = Ratio_Plot("LeadJetPt", njet, btag, [ht], 2, False)
-        # # plot.save(odir=outd)
-        # plot = Ratio_Plot("LeadJetEta", njet, btag, [ht], 2, False)
-        # plot.save(odir=outd)
-
-        # plot = Ratio_Plot("SecondJetPt", njet, btag, [ht], 1, False)
-        # plot.save(odir=outd)
-        # # plot = Ratio_Plot("SecondJetEta", njet, btag, [ht], 2, False)
-        # # plot.save(odir=outd)
-        # plot = Ratio_Plot("HT", njet, btag, [ht], 1, False)
-        # plot.save(odir=outd)
-        # plot = Ratio_Plot("MHT", njet, btag, [ht], 4, False)
-        # plot.save(odir=outd)
-        # # plot = Ratio_Plot("MET_Corrected", njet, btag, [ht], 8, False)
-        # # plot.save(odir=outd)
-
-        # # plot = Ratio_Plot("ComMinBiasDPhi_acceptedJets", njet, btag, [ht], 10, True)
-        # # plot.save(odir=outd)
-        # plot = Ratio_Plot("MHTovMET", njet, btag, [ht], 1, False)
-        # plot.save(odir=outd)
-        # plot = Ratio_Plot("EffectiveMass", njet, btag, [ht], 5, False)
-        # plot.save(odir=outd)
-
-        # # plot = Ratio_Plot("Number_Good_verticies", njet, btag, [ht], 1, False)
-        # # plot.save(odir=outd)
-        # plot = Ratio_Plot("JetMultiplicity", njet, btag, [ht], 1, False)
-        # plot.save(odir=outd)
-
-
-def do_all_plots_HT_incl(njet="le3j", btag="eq0b"):
-
-    # Custom bins for AlphaT per Rob's suggestion
-    b1 = np.arange(0.5, 1.0, 0.05)
-    b2 = np.arange(1.0, 4.5, 0.5)
-    alphaT_bins = np.concatenate((b1, b2))
-
-    # inclusive HT
-    rebin = 2
-    rebin_d = {"Number_Btags": 1, "JetMultiplicity": 1, "MHTovMET": 1,
-                "ComMinBiasDPhi_acceptedJets": 10, "AlphaT": alphaT_bins,
-                "MET_Corrected": 8, "HT": 5, "SecondJetPt": 4, "EffectiveMass": 10,
-                "MHT": 8, "LeadJetPt": 4}
-
-    log = False
-    log_these = ["AlphaT", "ComMinBiasDPhi_acceptedJets", "HT", "LeadJetPt", "SecondJetPt", "EffectiveMass"]
-    for v in plot_vars:
-        if v in rebin_d:
-            rebin = rebin_d[v]
-        else:
-            rebin = 2
-        if v in log_these:
-            log = True
-        else:
-            log = False
-        print v, njet, btag, HTbins
-        plot = Ratio_Plot(v, njet, btag, HTbins, rebin, log)
-        outd = "%s/%s_%s_%s" %(out_dir, njet, btag, "200_Inf")
-        plot.save(odir=outd)
+        plot.save()
+    # optionally can do component plots as well for this var
+    lo = HTbins[0].split("_")[0]
+    hi = HTbins[-1].split("_")[1] if "_" in HTbins[-1] else "Inf"
+    print "Make component pres"
+    pres.make_pres(plot_dir=out_dir, var=var, njet=njet, btag=btag, lo_ht=lo, hi_ht=hi)
 
 
 if __name__ == "__main__":
-    print "Making lots of data VS bg plots..."
-    make_plot_bins(plot_vars)
-    # do_all_plots_HT_excl()
-    # do_all_plots_HT_incl()
+    print "Making lots of data VS bg plots for inclusive HT..."
+    if len(sys.argv) == 5:
+        if sys.argv[-1] != "Inc" and sys.argv[-1] != "Exc":
+            print "Must be Inc or Exc"
+            exit(1)
+        if sys.argv[-1] == "Inc":
+            do_a_plot_HT_incl(var=sys.argv[1], njet=sys.argv[2], btag=sys.argv[3])
+        else:
+            do_a_plot_HT_excl(var=sys.argv[1], njet=sys.argv[2], btag=sys.argv[3])
+    elif len(sys.argv) == 2:
+        if sys.argv[-1] != "Inc" and sys.argv[-1] != "Exc":
+            print "Must be Inc or Exc"
+            exit(1)
+        for v, j, b in product(plot_vars, n_j, n_b):
+            if sys.argv[-1] == "Inc":
+                do_a_plot_HT_incl(var=v, njet=j, btag=b)
+            else:
+                do_a_plot_HT_excl(var=v, njet=j, btag=b)
+    else:
+        print "Run using:"
+        print "python shape_plots.py <Inc/Exc> # for all variable, njet, btag bins and whether Inclusive or Exclusive"
+        print "python shape_plots.py <variable> <njet> <btag> <Inc/Exc> # for one specific case"
+        exit(1)
