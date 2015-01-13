423be3fd082c9d7cd19c4e331a3ba8a7044b26c9
Robin Aggleton <robinaggleton@gmail.com>
2015-01-13 15:05:36 +0100

    use argparse to do user args better

--- shape_plots.py
+++ shape_plots.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 This script makes data VS background plots, but gets the backgrounds from
 DATA control region shape, not MC. To do this, we define several control regions
@@ -13,6 +14,7 @@ Robin Aggleton
 
 """
 
+import argparse
 import plot_grabber as grabr
 import ROOT as r
 from itertools import product, izip
@@ -24,7 +26,7 @@ from Ratio_Plot import Ratio_Plot
 import sys
 import make_component_pres as pres
 
-
+r.PyConfig.IgnoreCommandLineOptions = True
 r.TH1.SetDefaultSumw2(r.kFALSE)
 r.gStyle.SetOptStat(0)
 r.gROOT.SetBatch(1)
@@ -34,8 +36,8 @@ r.gStyle.SetOptFit(1111)
 # Define region bins
 allHTbins = ["200_275", "275_325", "325_375", "375_475", "475_575",
           "575_675", "675_775", "775_875", "875_975", "975_1075", "1075"][:]
-# n_j = ["le3j", "ge4j", "ge2j"][:2]
-n_j = ["eq2j", "eq3j", "eq4j", "ge5j"] # fine jet binning
+n_j = ["le3j", "ge4j", "ge2j"][:2]
+n_j_fine = ["eq2j", "eq3j", "eq4j", "ge5j"] # fine jet binning
 n_b = ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b", "ge1b"][:2]
 
 
@@ -44,11 +46,11 @@ n_b = ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b", "ge1b"][:2]
 ROOTdir, out_dir, HTbins = [
     ["/Users/robina/Dropbox/AlphaT/Root_Files_11Dec_aT_0p53_forRobin_v0/", "11Dec_aT_0p53_forRobin_v0_new", allHTbins[:]],  #re-run
     ["/Users/robina/Dropbox/AlphaT/Root_Files_11Dec_aT_0p53_forRobin_v0_MuonInJet/", "11Dec_aT_0p53_forRobin_v0_MuonInJet", allHTbins[3:]],  # muon in jet
-    ["/Users/robina/Dropbox/AlphaT/Root_Files_01Dec_aT_0p53_globalAlphaT_v1", "./01Dec_aT_0p53_globalAlphaT_v1/", allHTbins[3:]],  # alphaT in control regions as well
-    ["/Users/robina/Dropbox/AlphaT/Root_Files_04Dec_aT_0p53_fullHT_dPhi_lt0p3_v0", "./04Dec_aT_0p53_fullHT_dPhi_lt0p3_v0/", allHTbins[:]],  # dPhi* <0.3 in SR - old
+    ["/Users/robina/Dropbox/AlphaT/Root_Files_01Dec_aT_0p53_globalAlphaT_v1", "./01Dec_aT_0p53_globalAlphaT_v1_new/", allHTbins[3:]],  # alphaT in control regions as well
+    ["/Users/robina/Dropbox/AlphaT/Root_Files_04Dec_aT_0p53_fullHT_dPhi_lt0p3_v0", "./04Dec_aT_0p53_fullHT_dPhi_lt0p3_v0_new/", allHTbins[:]],  # dPhi* <0.3 in SR - old
     ["/Users/robina/Dropbox/AlphaT/Root_Files_21Dec_alphaT_0p53_fullHT_fixedCC_fineJetMulti_dPhi_gt0p3_v0", "./21Dec_alphaT_0p53_fullHT_fixedCC_fineJetMulti_dPhi_gt0p3_v0_new/", allHTbins[:]],  # dPhi* >0.3 in SR, fine jet multiplicity, fixed cross-cleaner
     ["/Users/robina/Dropbox/AlphaT/Root_Files_21Dec_alphaT_0p53_fullHT_fixedCC_fineJetMulti_dPhi_lt0p3_v0", "./21Dec_alphaT_0p53_fullHT_fixedCC_fineJetMulti_dPhi_lt0p3_v0_new/", allHTbins[:]]  # dPhi* <0.3 in SR, fine jet multiplicity, fixed cross-cleaner
-][-2]
+][0]
 
 # Variable(s) you want to plot
 plot_vars = ["Number_Btags", "AlphaT", "LeadJetPt", "LeadJetEta",
@@ -95,26 +97,39 @@ def do_a_plot_HT_excl(var="AlphaT", njet="le3j", btag="eq0b"):
 
 
 if __name__ == "__main__":
+    """
+    Mainly parse user args.
+
+    Note that by default, this script runs over all vars, all njet, all btag bins,
+    for INCLUSIVE HT. There are flags if you want to vary any of these.
+    """
+
     print "Making lots of data VS bg plots..."
-    if len(sys.argv) == 5:
-        if sys.argv[-1] != "Inc" and sys.argv[-1] != "Exc":
-            print "Must be Inc or Exc"
-            exit(1)
-        if sys.argv[-1] == "Inc":
-            do_a_plot_HT_incl(var=sys.argv[1], njet=sys.argv[2], btag=sys.argv[3])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--do_exclusive_HT", help="if you want plots done in HT bins rather than for inclusive HT", action='store_true', default=False)
+    parser.add_argument("-v", "--var", help=("variable to plot (if undefined, runs over all)"), nargs="+")
+    parser.add_argument("-j", "--njet", help=("number of jets (if undefined, runs over all)"), nargs="+")
+    parser.add_argument("-b", "--btag", help=("number of btags (if undefined, runs over all)"), nargs="+")
+    args = parser.parse_args()
+
+    # Figure out which vars/njet/btags options to run over
+    # Do the set intersection to validate input (silently tho, tut tut)
+    # If user specified, then run over those only, if not then all possible
+    run_vars = plot_vars[:]
+    run_njet = n_j_fine[:] if "fineJetMulti" in ROOTdir else n_j[:]
+    run_btag = n_b[:]
+    if args.var:
+        run_vars = list(set(plot_vars) & set(args.var))
+    if args.njet:
+        run_njet = list(set(run_njet) & set(args.njet))
+    if args.btag:
+        run_btag = list(set(n_b) & set(args.btag))
+
+    print run_vars, run_njet, run_btag
+
+    for v, j, b in product(run_vars, run_njet, run_btag):
+        if args.do_exclusive_HT:
+            do_a_plot_HT_excl(var=v, njet=j, btag=b)
         else:
-            do_a_plot_HT_excl(var=sys.argv[1], njet=sys.argv[2], btag=sys.argv[3])
-    elif len(sys.argv) == 2:
-        if sys.argv[-1] != "Inc" and sys.argv[-1] != "Exc":
-            print "Must be Inc or Exc"
-            exit(1)
-        for v, j, b in product(plot_vars, n_j, n_b):
-            if sys.argv[-1] == "Inc":
-                do_a_plot_HT_incl(var=v, njet=j, btag=b)
-            else:
-                do_a_plot_HT_excl(var=v, njet=j, btag=b)
-    else:
-        print "Run using:"
-        print "python shape_plots.py <Inc/Exc> # for all variable, njet, btag bins and whether Inclusive or Exclusive"
-        print "python shape_plots.py <variable> <njet> <btag> <Inc/Exc> # for one specific case"
-        exit(1)
+            do_a_plot_HT_incl(var=v, njet=j, btag=b)
