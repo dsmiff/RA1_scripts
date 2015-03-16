99e2453da150252a810c52f38ae7517252d8a7d5
Robin Aggleton <robin.aggleton@cern.ch>
2015-03-16 21:15:29 +0000

    more condor scripts

--- shape_plots_condor.py
+++ shape_plots_condor.py
@@ -7,9 +7,7 @@ Usage: call in HTcondor script with arguments:
 $(process) ./myrootfiles ./myoutdir --excl
 """
 
-import shape_plots as plotr
 import argparse
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("ID", type=int, help="ID number to specify variable")
@@ -18,9 +16,10 @@ parser.add_argument("OUTdir", help="output dir")
 parser.add_argument("--excl", action="store_true",
                     help="run exclusive HT bins (default is inclusive HT)")
 args = parser.parse_args()
+print args
 
-
-var = plotr.plot_vars[ID:ID + 1]
+import shape_plots as plotr
+var = plotr.plot_vars[args.ID:args.ID + 1]
 plotr.run_over(root_dir=args.ROOTdir, out_dir=args.OUTdir, plot_vars=var,
                njet=plotr.n_j, btag=plotr.n_b, htbins=plotr.HTbins,
                exclusive_HT=args.excl, check=False, custom_title=plotr.title)
---
+++ shape_plots_condor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+host=`hostname`
+# some information for debugging
+echo "I am running on $host"
+echo "I got the following parameters: $@"
+# source CMSSW env
+. $VO_CMS_SW_DIR/cmsset_default.sh
+# get CMSSW
+scramv1 project CMSSW CMSSW_7_3_0
+cd CMSSW_7_3_0/src/
+eval `scramv1 runtime -sh`
+cd ../..
+ls
+python shape_plots_condor.py $@
